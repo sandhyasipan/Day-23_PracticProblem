f95bc0bef1f5cfc7c004ffc44cd4fd9157f6bdcc
sandhyasipan <sandhyaswain70@gmail.com>
2022-09-13 20:36:32 +0530

    added to UC4_MobileNo

---
+++ MobileNumber.java
@@ -0,0 +1,20 @@
+package com.bridgelabs;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileNumber {
+    public static boolean isValidUsername(String mobileNo) {
+        String regex = "^[0-9]{2}\\s[1-9]{1}[0-9]{9}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(mobileNo);
+        return matcher.matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your mobile number : ");
+        String str = sc.nextLine();
+        System.out.println(isValidUsername(str));
+    }
+}

ad871812eb335a6671d80067710c5e6f8f065d7f
sandhyasipan <sandhyaswain70@gmail.com>
2022-09-13 20:49:39 +0530

    added to UC5_Password

---
+++ Password.java
@@ -0,0 +1,20 @@
+package com.bridgelabs;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Password {
+    public static boolean isValidUsername(String password) {
+        String regex = "^[A-Za-z]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your password : ");
+        String str = sc.nextLine();
+        System.out.println(isValidUsername(str));
+    }
+}

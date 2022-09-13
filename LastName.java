495748ac9fdf4510a578adc49dc4924995439909
sandhyasipan <sandhyaswain70@gmail.com>
2022-09-13 20:23:11 +0530

    added to UC2_LastName

---
+++ LastName.java
@@ -0,0 +1,20 @@
+package com.bridgelabs;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LastName {
+    public static boolean isValidUsername(String lname) {
+        String regex = "^[A-Z][a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lname);
+        return matcher.matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your last name : ");
+        String str = sc.nextLine();
+        System.out.println(isValidUsername(str));
+    }
+}
